823b2c54b20ed015ddb520757ba47a85d92202f0
imran-py <imran.dtc144@gmail.com>
2023-11-05 18:26:47 +0530

    post api

--- src/App.js
+++ src/App.js
@@ -5,38 +5,17 @@ const App = () => {
   const [data,setData] = useState([])
 
   useEffect(()=>
-    axios.get('https://reqres.in/api/users?page=2')
-    .then(res=>setData(res.data.data))
+    axios.post('https://reqres.in/api/users',{
+      "name": "imran",
+      "job": "fullstack developer"
+    })
+    .then(res=>console.log(res.data))
     .catch(err=>console.log(err)),[]
   )
   return (
     <div>
-      <h1>please check your console for get api response</h1>
-      {
-        data.map((d,i)=>{
-          // return <p key={id}>{d.email}</p>
-          return <table align='center'>
-            <thead>
-              <tr>
-                <th>id</th>
-                <th>firstname</th>
-                <th>lastname</th>
-                <th>email</th>
-                <th>img path</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>{d.id}</td>
-                <td>{d.first_name}</td>
-                <td>{d.last_name}</td>
-                <td>{d.email}</td>
-                <td>{d.avatar}</td>
-              </tr>
-            </tbody>
-          </table>  
-        })
-      }
+      <h1>please check your console for post api response</h1>
+      
     </div>
   )
 }
