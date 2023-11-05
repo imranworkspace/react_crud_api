c05bb4f8a664e64ab320a3fabe44cecdb1b9fac8
imran-py <imran.dtc144@gmail.com>
2023-11-05 17:57:12 +0530

    dynamic data showed into UI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const App = () => {
+  const [data,setData] = useState([])
+
   useEffect(()=>
     axios.get('https://reqres.in/api/users?page=2')
-    .then(res=>console.log(res))
+    .then(res=>setData(res.data.data))
     .catch(err=>console.log(err)),[]
   )
   return (
     <div>
       <h1>please check your console for get api response</h1>
+      {
+        data.map((d,id)=>{
+          return <p key={id}>{d.email}</p>  
+        })
+      }
     </div>
   )
 }
